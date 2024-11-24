e19cac841999692d5162efe11ef89c6d9d1e9ee8
likerhythm <wjdalsdl0412@gmail.com>
2024-11-24 16:28:41 +0900

    feat(menu mapper): menu mapper 기능

--- src/main/java/christmas/MenuMapper.java
+++ src/main/java/christmas/MenuMapper.java
@@ -2,6 +2,7 @@ package christmas;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class MenuMapper {
@@ -12,7 +13,17 @@ public class MenuMapper {
         this.menuRepository = menuRepository;
     }
 
-    public Map<Menu, Integer> mapToMenu(Map<String, String> parsedOrderInput) {
+    public Map<Menu, Integer> mapParsedOrderInput(Map<String, String> parsedOrderInput) {
+        LinkedHashMap<Menu, Integer> menuAndQuantity = createMap(parsedOrderInput);
+        boolean isAllValidQuantity = menuAndQuantity.values().stream()
+                .allMatch(v -> v > 0);
+        if (!isAllValidQuantity) {
+            throw new IllegalArgumentException("유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+        return menuAndQuantity;
+    }
+
+    private LinkedHashMap<Menu, Integer> createMap(Map<String, String> parsedOrderInput) {
         return parsedOrderInput.entrySet().stream()
                 .collect(Collectors.toMap(
                         entry -> menuRepository.findByName(entry.getKey()),
